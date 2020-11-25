3ddb63da6d684139c82afda5d3934e1791fe49b2
vehave <verneri.hartus@gmail.com>
2020-11-25 18:46:53 +0200

    anekdootit

---
+++ routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useField = (type) => {  
+    const [value, setValue] = useState('')
+    
+    
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+    
+    const empty = (event) => {
+        setValue(event.target.value)
+        setValue('')
+    }
+
+    return {
+        type,
+        value,
+        onChange,
+        empty
+    }
+}
+
